252b739d519eca7fe08a80148528678ed2651413
unknown <thrrltk321@gmail.com>
2025-03-30 13:36:45 +0900

    Fix: use MONGO_URI for Render deployment

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ if(myArgs.heroku){
   mongoDBName = 'phaserQuest';
 }
 
-mongoose.connect('mongodb://localhost:27017/phaserQuest', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
